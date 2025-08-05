02184a74492e1ccd7da25c219777d3d78f58c6c5
HarshithaPaidi05 <harshithapaidi80@gmail.com>
2025-08-05 11:46:32 +0530

    Add React Week 7 cricketapp excluding node_modules

---
+++ cricketapp/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ListOfPlayers from './components/ListOfPlayers';
+import IndianPlayers from './components/IndianPlayers';
+
+function App() {
+  return (
+    <div>
+      <ListOfPlayers />
+      <IndianPlayers />
+    </div>
+  );
+}
+
+export default App;
